213b27ddf382f99172bf54f87cf3381eacd631ec
Abe Yoshida <abeyoshida@gmail.com>
2020-06-22 11:08:06 -0400

    remove url strings and use process.env variable for database url

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,16 +5,13 @@ const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.FIREBASE_DATABASE_URL, 
-    // dev: 'https://expensify-e8c07.firebaseio.com',  
-    // test: 'https://expensify-test-92579.firebaseio.com'
-    // process.env.FIREBASE_DATABASE_URL,
     projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.FIREBASE_APP_ID,
     measurementId: process.env.FIREBASE_MEASURMENT_ID
   };
-console.log('firebase.js firebaseConfig: ', firebaseConfig);
+
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   const fdb = firebase.database();

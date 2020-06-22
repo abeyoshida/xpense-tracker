9d3638b280f1ed4cdaa0ee3b5a814e5fc3b031c3
Abe Yoshida <abeyoshida@gmail.com>
2020-06-22 12:29:52 -0400

    clean up firebaseConfig

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,7 +5,7 @@ const env = 'test';  // 'test' or 'dev'
 const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: (env === 'dev') ? 'https://expensify-e8c07.firebaseio.com' : 'https://expensify-test-92579.firebaseio.com',  // process.env.FIREBASE_DATABASE_URL, 
+    databaseURL: process.env.FIREBASE_DATABASE_URL, 
     projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
